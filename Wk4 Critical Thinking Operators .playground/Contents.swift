71b0627a3821ed8354aa2f868afcb4cb2c3a6fd6
Edgar Ali <89033771+EChavez12@users.noreply.github.com>
2025-05-14 19:27:05 -0500

    Add files via upload

---
+++ Wk4 Critical Thinking Operators .playground/Contents.swift
@@ -0,0 +1,28 @@
+import UIKit
+
+var greeting = "Hello, playground this is my operator app"
+
+// Critical Thinking Operators Exercise
+
+let text = "Hello, I'm learning "
+let text2 = "on how operators work"
+
+let text3 = (text + text2)
+
+let add = 18;
+let num = 22;
+let a = 9;
+let b = 21;
+
+let addition = (add + num);
+let c = (a-b)
+let mult = (a * b)
+let div = (add/num)
+
+print("Text: ",text3, "\nAddition 18 + 22:",addition,"\nMinus 9 - 21:",c)
+
+num >= add
+add <= a
+
+c != a
+a == num
