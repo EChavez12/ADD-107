24d69199732c65601acab0611eb6bc2143b4f3b2
Edgar Ali <89033771+EChavez12@users.noreply.github.com>
2025-05-14 19:01:23 -0500

    Add files via upload

---
+++ Wk4 Operator Exercise Calc.playground/Contents.swift
@@ -0,0 +1,21 @@
+import UIKit
+
+let greeting = "Hello, playground this is my calculator"
+
+// Top section: will define all input variables.
+let a = 10;
+let b = 15;
+let c = 35;
+let d = 50;
+let e = 22;
+let f = 8;
+
+// Middle Section: Defines all the operations.
+let plus = (a + e);
+let minus = (d - b);
+let mult = (c * f);
+let div = (d / f);
+let percent = (c % b);
+
+// Bottom Section: Will print all the results.
+print("Plus:", plus, "\nMinus:", minus, "\nMultiplication:", mult, "\nDivision:", div, "\nModulus:", percent)
